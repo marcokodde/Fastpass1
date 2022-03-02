341574ebb241c8a08764cecd8eec095446a8cb07
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-02 14:33:47 -0700

    Modelo Garage: Métodos para saber espacios disponibles (numérico), ocupados (numérico) y si hay o no disponbiles (true/false) para vehículos con enganche adicional

--- app/Models/Garage.php
+++ app/Models/Garage.php
@@ -26,22 +26,38 @@ class Garage extends Model
      * +------------+
      */
 
+
+     // Tiene espacio (no importa si es o no de enganche adicional)
      public function has_space(){
         return $this->vehicles_in_garages->count() < ENV('GARAGE_SPACES');
      }
 
+     // Espacios ocupados (No importa si es o no de enganche adicional)
      public function occupied_spaces(){
         return $this->vehicles_in_garages->count();
      }
 
+     // Espacios disponibles (No importa si es o no de enganche adicional)
      public function available_spaces(){
          return ENV('GARAGE_SPACES') - $this->vehicles_in_garages->count();
      }
 
-
-     public function has_limit_vehicle_from_next_tier(){
-        return $this->vehicles_in_garages->count >= env('APP_MAX_VEHICLE_IN_GARAGE_NEXT_TIER',1);
+     // Espacios ocupados (De Enganche Adicional)
+     public function occupied_spaces_like_next_tier(){
+         return $this->vehicles_in_garages->where('is_additional_next_tier',1)->count();
      }
+
+     // Espacios disponibles para Enganche Adicional
+     public function available_spaces_like_next_tier(){
+        return ENV('APP_MAX_VEHICLE_IN_GARAGE_NEXT_TIER') - $this->vehicles_in_garages->where('is_additional_next_tier',1)->count();
+     }
+
+     // ¿Tiene espacio para vehículo con Enganche Adicional?
+     public function has_space_to_next_tier(){
+        return $this->vehicles_in_garages->where('is_additional_next_tier',1)->count() < ENV('GARAGE_SPACES_TO_NEXT_TIER');
+     }
+
+
 
     /**+------------+
      * | Búsquedas  |
