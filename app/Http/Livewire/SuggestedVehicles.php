13a219a6d80a1f95b9ac69f7a228ed7d3ae25502
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-03 13:17:09 -0700

    Botón para ver garage en misma vista que la de sugeridos

--- app/Http/Livewire/SuggestedVehicles.php
+++ app/Http/Livewire/SuggestedVehicles.php
@@ -26,6 +26,8 @@ class SuggestedVehicles extends Component
     public $downpayment = 0;
     public $downpayment_ranges = [];
     public $client_has_vehicles_with_downpayment=false;
+    public $show_garage = false;
+    public $header_page = 'Vehicles you are approved';
 
 
     public function mount(){
@@ -45,17 +47,34 @@ class SuggestedVehicles extends Component
         if($this->read_neo_api && $this->client){
             $this->load_suggested_vehicles();
         }
-        $this->client_has_vehicles_with_downpayment = $this->client->has_vehicles_with_downpayment();
-        $this->records = $this->read_suggested_vehicles_client_id($this->client->id,$this->downpayment); // Lee sugeridos del cliente;
+
+
         if($this->garage){
             $this->review_garage();
         }
 
-        return view('livewire.suggested_vehicles.vehicles');
+        if($this->show_garage && $this->garage){
+            $this->records = $this->garage->vehicles_in_garages()->get();
+            $this->client_has_vehicles_with_downpayment = false;
+            $this->header_page = 'Vehicles in my Garage';
+            $this->records = $this->read_suggested_vehicles_client_id($this->client->id,$this->downpayment); // Lee sugeridos del cliente;
+            return view('livewire.suggested_vehicles.vehicles');
+        }else{
+            $this->header_page = 'Vehicles you are approved';
+            $this->client_has_vehicles_with_downpayment = $this->client->has_vehicles_with_downpayment();
+            $this->records = $this->read_suggested_vehicles_client_id($this->client->id,$this->downpayment); // Lee sugeridos del cliente;
+            return view('livewire.suggested_vehicles.vehicles');
+        }
+
+
+
+
     }
 
 
-
+    public function set_show_garage(){
+        $this->show_garage = !$this->show_garage;
+    }
 
 
     public function ok() { dd("okokoko");}
