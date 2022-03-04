ba0c6990be2e1f7ffa7003380e388201b89c1c43
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-04 15:41:48 -0700

    Sincronizacion

--- database/migrations/2022_02_24_000000_create_clients_table.php
+++ database/migrations/2022_02_24_000000_create_clients_table.php
@@ -16,11 +16,7 @@ class CreateClientsTable extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('client_id',50)->comment('Id Cliente Neo');
-<<<<<<<< HEAD:database/migrations/2022_02_24_000000_create_clients_table.php
-            $table->integer('loggin_times')->default(0);
-========
             $table->integer('loggin_times')->default(0)->comment('Vecese que ha ingresado');
->>>>>>>> ad846dbf98090509e31d58834ae4d88cfc1879e9:database/migrations/2022_02_24_172000_create_clients_table.php
         });
     }
 
