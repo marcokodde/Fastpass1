ae68c639c56e29cbae90d62997866464c9086e6b
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-05 13:01:14 -0700

    Se agrega icono en parte superior derecha con la burbuja que indica la cantidad de espacios ocupados en el garaje

--- resources/views/livewire/suggested_vehicles/index.blade.php
+++ resources/views/livewire/suggested_vehicles/index.blade.php
@@ -1,3 +1,12 @@
-<div class="ml-4">
+<div class="flex justify-between ml-4 mr-10">
     <img class="stnd skip-lazy dark-version" width="207" height="110" alt="CTC Auto Group" src="https://149646797.v2.pressablecdn.com/wp-content/uploads/2021/05/brand-logo.png"/>
+    <span class="relative inline-block mt-5">
+
+        <button wire:click="$toggle('show_garage')" title="{{__('Show My Garage')}}">
+            <img src="{{asset('images/garage.png')}}" height="60px" width="60px">
+            <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+                {{$garage->occupied_spaces()}}
+            </span>
+        </button>
+      </span>
 </div>
