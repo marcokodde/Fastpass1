5fc065e02677dfb88b7532a65c9eeb9c28442faf
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-08 23:16:08 -0700

    Agregar desde vehículos adicionales ya funciona

--- resources/views/livewire/new_show_vehicles/list_additionals.blade.php
+++ resources/views/livewire/new_show_vehicles/list_additionals.blade.php
@@ -50,23 +50,7 @@
     @endif
 
     <div class="mb-2">
-        @if ($garage && !$garage->available_spaces_like_next_tier() && $garage->is_vehicle_in_garage($record->inventory->stock))
-                <button disabled title="{{__('Vehicle Added')}}"
-                type="button" class="bg-gray-600 text-white px-4 pb-2 py-2 m-2 rounded-lg relative uppercase">
-                {{__('Added To Garage')}}
-                </button>
-        @elseif ($garage && $garage->occupied_spaces_like_next_tier() && !$garage->available_spaces_like_next_tier() && $garage->not_available_spaces_like_next_tier())
-            <button disabled title="{{__('Garage Full')}}"
-                type="button" class="bg-gray-500 text-white px-8 pb-4 py-4 m-4 rounded-lg relative uppercase">
-                {{__('Garage Full')}}
-            </button>
-        @else
-            <button type="button" style="background-color:#6AB04C"
-                    wire:click.prevent="$emit('add_to_garage', '{{$record->inventory->stock}}' )"
-                    class="text-black px-8 pb-4 py-4 m-4 rounded-lg relative uppercase">
-                {{__('Add To Garage')}}
-            </button>
-        @endif
+       @include('livewire.new_show_vehicles.button_to_add_vehicle')
     </div>
-    @include('common.faspass')
+
 </div>
