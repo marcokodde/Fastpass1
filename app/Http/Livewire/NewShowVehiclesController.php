cfeb7783aeb292955ead9b11aa9cf2e285fd4df6
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-09 09:41:00 -0700

    Estilos personalizados CSS para botones en alertas de Sweet Alert

--- app/Http/Livewire/NewShowVehiclesController.php
+++ app/Http/Livewire/NewShowVehiclesController.php
@@ -59,7 +59,7 @@ class NewShowVehiclesController extends Component
 
 
         if ($this->garage && $this->show_garage ) {
-            $this->show_additional = false;
+            $this->show_garage = false;
             $this->read_garage();
             $this->header_page = 'My Garage';
             $this->view_to_show = $this->view_to_show = 'livewire.new_show_vehicles.list_garage';
