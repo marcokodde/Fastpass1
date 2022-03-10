4969549009195a5ae9598ceec247e4f61ff9e2a0
marcokodde <marco@koddeplus.com>
2022-03-10 11:48:06 -0600

    Agregando mejora en vista welcome, pendientes para Fastpass y slider en testcontroller

--- resources/views/livewire/new_show_vehicles/no_active_session.blade.php
+++ resources/views/livewire/new_show_vehicles/no_active_session.blade.php
@@ -1,11 +1,13 @@
 <div>
-    @include('livewire.show_vehicles.index')
+    <div class="flex left-0">
+        <img class="stnd skip-lazy dark-version" width="207" height="110" alt="CTC Auto Group" src="https://149646797.v2.pressablecdn.com/wp-content/uploads/2021/05/brand-logo.png"/>
+    </div>
     <div class="sidemenu mt-12 w-64 absolute">
     </div>
     <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            {{-- TODO: Mejorar la presentación de este mensaje --}}
-            NO EXISTE EL CLIENTE O FALTAN DATOS DE CONEXION
+            {{-- TODO: Mejorar la presentaci��n de este mensaje --}}
+            <h3 class="mx-auto text-center">{{__('Error establishing the connection. Check the link and try again.')}}</h3>
         </div>
     </div>
 
