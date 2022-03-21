b52a00d932479b4f44fa2c8009cd755f54130bce
marcokodde <marco@koddeplus.com>
2022-03-21 15:19:04 -0600

    Detalle en js

--- public/js/confetti.js
+++ public/js/confetti.js
@@ -44,7 +44,7 @@ var removeConfetti; //call to stop the confetti animation and remove all confett
 		if (canvas === null) {
 			canvas = document.createElement("canvas");
 			canvas.setAttribute("id", "confetti-canvas");
-			canvas.setAttribute("style", "display:none; margin-top:-450px; opacity:0.9;pointer-events:none");
+			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
 			document.body.appendChild(canvas);
 			canvas.width = width;
 			canvas.height = height;
