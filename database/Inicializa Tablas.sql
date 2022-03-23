913e2dfb69332c3827cd30c9b9c0bad71cedf321
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-23 14:25:11 -0700

    Se crean arcivos para consultas generales

---
+++ database/Inicializa Tablas.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE detail_garages;
+TRUNCATE suggested_vehicles;
+TRUNCATE garages;
+TRUNCATE client_sessions;
+TRUNCATE clients; 
+TRUNCATE dealers;
+SET FOREIGN_KEY_CHECKS = 1;
+
--- database/consultas.sql
+++ database/consultas.sql
@@ -1,12 +1,15 @@
 /** Consulta General */
-
-SELECT cl.client_id,cl.downpayment,cl.loggin_times,
-       cs.expire_at,cs.token,
+SELECT sv.id,cl.id,cl.client_id,cl.downpayment,cl.loggin_times,
+       cs.expire_at,cs.token,cs.has_been_used,
 		 de.name,de.percentage,
-		 inv.stock,inv.vin,inv.make,inv.model,
+		 inv.stock,inv.vin,inv.make,inv.model,inv.retail_price,
 		 sv.sale_price,sv.grade,sv.downpayment_for_next_tier
 FROM clients cl,client_sessions cs,dealers de,inventories inv,suggested_vehicles sv
 WHERE cl.id =cs.client_id
   AND cl.id = sv.client_id
   AND de.id = sv.dealer_id
   AND inv.id = sv.inventory_id
+  AND cl.id  = 2
+ -- AND inv.id = 627
+ -- AND downpayment_for_next_tier
+ORDER BY sv.sale_price
