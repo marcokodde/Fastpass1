80071c73188dcaa60b34ea78997b2fec594a011f
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-25 15:57:09 -0700

    WelcomeController: Se envía a una sola vista, si están mal los parámetros en ella se decie que presentar, se crea método para enviar nota con nuevo URL al cliente incluyendo cliente y token

--- resources/views/livewire/welcome/welcome-bad-params.blade.php
+++ resources/views/livewire/welcome/welcome-bad-params.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-            <img class="stnd skip-lazy dark-version" width="207" height="110" alt="CTC Auto Group" src="https://149646797.v2.pressablecdn.com/wp-content/uploads/2021/05/brand-logo.png"/>
+        <img class="stnd skip-lazy dark-version" width="207" height="110" alt="CTC Auto Group" src="https://149646797.v2.pressablecdn.com/wp-content/uploads/2021/05/brand-logo.png"/>
         <div style="background-color: #6AB04C">
             <div style="background-color: #6AB04C" class="relative flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div class="mx-auto block 2xl:-mt-12 mt-0 md:mt-1">
