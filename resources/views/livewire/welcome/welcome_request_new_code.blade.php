2900244e47220e218dfd41991d7551454d3d91dc
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-03-28 16:51:21 -0700

    Cambiar imagen para crear código

--- resources/views/livewire/welcome/welcome_request_new_code.blade.php
+++ resources/views/livewire/welcome/welcome_request_new_code.blade.php
@@ -2,7 +2,7 @@
     <img class="stnd skip-lazy dark-version" width="207" height="110" alt="CTC Auto Group" src="https://149646797.v2.pressablecdn.com/wp-content/uploads/2021/05/brand-logo.png"/>
     <div style="background-color: #6AB04C" class="relative flex items-center justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div class="mx-auto block 2xl:-mt-44 mt-0 md:mt-1 sm:mt-0">
-            <img style="height:70%; width:70%;" class="mx-auto" src="{{asset('images/lets_driving.png')}}">
+            <img style="height:70%; width:70%;" class="mx-auto" src="{{asset('images/session_expire_garage.png')}}">
             <button wire:click.prevent="client_require_new_code({{$client}})" onclick="execute()"
                     style="background-color:#f1c40f" class="btn_reques_code mx-auto text-white hover:text-black">
                 {{ __('REQUEST NEW CODE')}}
@@ -13,7 +13,7 @@
 <script>
     function execute() {
         Swal.fire({
-            title: 'Good Job!, Let"s get you Driving!',
+            title: 'Your purchase process is almost done, wait for an email or text to get a new code',
             width: 450,
             padding: '3em',
             width: 400,
@@ -28,3 +28,4 @@
         })
     }
 </script>
+
