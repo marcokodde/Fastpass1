03b2a469f4b87d4fb118e0a5b502dc91685de2c6
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-14 14:25:06 -0600

    Mostrar variable $_SERVER desde una API/server_data

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/support/server/server_data/all',function(){
+    echo '<table>';
+    echo '<thead border="1"><th>CLAVE</th><th>VALOR</th></tehead>';
+    foreach($_SERVER as $server => $valor){
+        echo '<tr><td> ' . $server . '</td><td>' . $valor . '</td></tr>';
+    }
+    echo '</table>';
+});
+
+Route::get('/server_data',function(){
+    echo '<table>';
+    echo '<thead border="1"><th>CLAVE</th><th>VALOR</th></tehead>';
+    foreach($_SERVER as $server => $valor){
+        if (is_array($valor)){
+            echo '<tr><td> ' . $server . '</td><td>';
+            foreach($valor as $x_valor => $valor_x){
+                echo '<tr><td>' . $x_valor .'</td><td>' . $valor_x . '</td></tr>';
+            }
+            echo '</td>';
+        }else{
+            echo '<tr><td> ' . $server . '</td><td>' .$valor . '</td></tr>';
+        }
+    }
+    echo '</table>';
+});
